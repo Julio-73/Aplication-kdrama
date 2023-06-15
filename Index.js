90838cee5ae13f22892202ba4d41f031ed2549bd
julio-cesar <112985882+Julio-73@users.noreply.github.com>
2023-06-15 02:25:00 -0500

    primer-commit

---
+++ Index.js
@@ -0,0 +1,33 @@
+let modo = document.getElementById("modo");
+
+let body = document.body;
+
+modo.addEventListener("click", function () {
+  let val = body.classList.toggle("dark");
+  localStorage.setItem("modo", val);
+});
+
+let valor = localStorage.getItem("modo");
+
+if (valor === "true") {
+  body.classList.add("dark");
+} else {
+  body.classList.remove("dark");
+}
+
+// scroll
+
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 1000,
+  delay: 100,
+});
+
+ScrollReveal().reveal(".reparto-1", { origin: "left" });
+
+ScrollReveal().reveal(".Genero-1", { origin: "left" });
+
+ScrollReveal().reveal(".sound", { origin: "top" });
+
+ScrollReveal().reveal(".capitulo", { origin: "top" });
